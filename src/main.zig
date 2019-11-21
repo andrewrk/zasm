e55a5191cccc17e8abd2a549f329a4415e28b071
Andrew Kelley <andrew@ziglang.org>
2019-11-20 22:44:18 -0500

    cli requires cmd args

--- src/main.zig
+++ src/main.zig
@@ -6,13 +6,21 @@ const fs = std.fs;
 const process = std.process;
 const math = std.math;
 
+const Cmd = enum {
+    exe,
+    obj,
+    dis,
+    targets,
+};
+
 pub fn main() anyerror!void {
     var arena = std.heap.ArenaAllocator.init(std.heap.direct_allocator);
     defer arena.deinit();
     const allocator = &arena.allocator;
 
-    var target = std.Target.Native;
+    var target: std.Target = .Native;
     var input_files = std.ArrayList([]const u8).init(allocator);
+    var maybe_cmd: ?Cmd = null;
 
     const args = try process.argsAlloc(allocator);
     var arg_i: usize = 1;
@@ -29,14 +37,50 @@ pub fn main() anyerror!void {
                 std.debug.warn("Invalid parameter: {}\n", full_arg);
                 dumpStdErrUsageAndExit();
             }
+        } else if (maybe_cmd == null) {
+            inline for (std.meta.fields(Cmd)) |field| {
+                if (mem.eql(u8, full_arg, field.name)) {
+                    maybe_cmd = @field(Cmd, field.name);
+                    break;
+                }
+            } else {
+                std.debug.warn("Invalid command: {}\n", full_arg);
+                dumpStdErrUsageAndExit();
+            }
         } else {
             try input_files.append(full_arg);
         }
     }
 
+    const cmd = maybe_cmd orelse {
+        std.debug.warn("Expected a command parameter\n");
+        dumpStdErrUsageAndExit();
+    };
+
+    switch (cmd) {
+        .targets => {
+            try std.io.getStdOut().write(
+                \\x86_64-linux
+                \\
+            );
+            return;
+        },
+        .exe => return assembleExecutable(allocator, input_files.toSliceConst(), target),
+        .obj => {
+            std.debug.warn("object files not yet implemented\n");
+            process.exit(1);
+        },
+        .dis => {
+            std.debug.warn("disassembly not yet implemented\n");
+            process.exit(1);
+        },
+    }
+}
+
+fn assembleExecutable(allocator: *mem.Allocator, input_files: []const []const u8, target: std.Target) !void {
     const cwd = fs.Dir.cwd();
 
-    for (input_files.toSliceConst()) |input_file| {
+    for (input_files) |input_file| {
         const source = try cwd.readFileAlloc(allocator, input_file, math.maxInt(usize));
         var tokenizer = Tokenizer.init(source);
         while (true) {
