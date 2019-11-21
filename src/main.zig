891672c5a8891076d9bbbb5313de07a5e973d1fe
Andrew Kelley <andrew@ziglang.org>
2019-11-21 00:22:06 -0500

    handling basic directives

--- src/main.zig
+++ src/main.zig
@@ -17,11 +17,15 @@ const Assembly = struct {
     allocator: *mem.Allocator,
     input_files: []const []const u8,
     target: std.Target,
-    current_section: ?*Section,
     errors: std.ArrayList(Error),
 
     const Error = union(enum) {
         unexpected_token: struct {
+            token: Token,
+            source: []const u8,
+            file_name: []const u8,
+        },
+        unrecognized_directive: struct {
             token: Token,
             source: []const u8,
             file_name: []const u8,
@@ -37,6 +41,16 @@ const Assembly = struct {
                         loc.line + 1,
                         loc.column + 1,
                         @tagName(unexpected_token.token.id),
+                    );
+                },
+                .unrecognized_directive => |info| {
+                    const loc = tokenLocation(info.source, info.token);
+                    try stream.print(
+                        "{}:{}:{}: error: unrecognized directive: {}\n",
+                        info.file_name,
+                        loc.line + 1,
+                        loc.column + 1,
+                        info.source[info.token.start..info.token.end],
                     );
                 },
             }
@@ -77,10 +91,6 @@ fn tokenLocation(source: []const u8, token: Token) Location {
     return loc;
 }
 
-const Section = struct {
-    name: []const u8,
-};
-
 pub fn main() anyerror!void {
     var arena = std.heap.ArenaAllocator.init(std.heap.direct_allocator);
     defer arena.deinit();
@@ -90,7 +100,6 @@ pub fn main() anyerror!void {
         .allocator = allocator,
         .target = .Native,
         .input_files = undefined,
-        .current_section = null,
         .errors = std.ArrayList(Assembly.Error).init(allocator),
     };
     var input_files = std.ArrayList([]const u8).init(allocator);
@@ -162,22 +171,140 @@ pub fn main() anyerror!void {
     }
 }
 
+const AsmFile = struct {
+    source: []const u8,
+    file_name: []const u8,
+    tokenizer: Tokenizer,
+    assembly: *Assembly,
+    current_section: ?*Section = null,
+    sections: SectionTable,
+    globals: GlobalSet,
+    put_back_buffer: [1]Token,
+    put_back_count: u1,
+
+    const SectionTable = std.StringHashMap(*Section);
+    const GlobalSet = std.StringHashMap(void);
+
+    const Section = struct {
+        name: []const u8,
+    };
+
+    fn tokenSlice(asm_file: AsmFile, token: Token) []const u8 {
+        return asm_file.source[token.start..token.end];
+    }
+
+    fn findOrCreateSection(self: *AsmFile, name: []const u8) !*Section {
+        const gop = try self.sections.getOrPut(name);
+        if (gop.found_existing) {
+            return gop.kv.value;
+        }
+        const section = try self.sections.allocator.create(Section);
+        section.* = Section{
+            .name = name,
+        };
+        gop.kv.value = section;
+        return section;
+    }
+
+    fn setCurrentSection(self: *AsmFile, name: []const u8) !void {
+        self.current_section = try self.findOrCreateSection("text");
+    }
+
+    fn addGlobal(self: *AsmFile, name: []const u8) !void {
+        _ = try self.globals.put(name, {});
+    }
+
+    fn nextToken(self: *AsmFile) Token {
+        if (self.put_back_count == 0) {
+            return self.tokenizer.next();
+        } else {
+            self.put_back_count -= 1;
+            return self.put_back_buffer[self.put_back_count];
+        }
+    }
+
+    fn eatToken(self: *AsmFile, id: Token.Id) ?Token {
+        const token = self.nextToken();
+        if (token.id == id) return token;
+        self.putBackToken(token);
+        return null;
+    }
+
+    fn putBackToken(self: *AsmFile, token: Token) void {
+        self.put_back_buffer[self.put_back_count] = token;
+        self.put_back_count += 1;
+    }
+
+    fn expectToken(asm_file: *AsmFile, id: Token.Id) !Token {
+        const token = asm_file.nextToken();
+        if (token.id != id) {
+            try asm_file.assembly.errors.append(.{
+                .unexpected_token = .{
+                    .token = token,
+                    .source = asm_file.source,
+                    .file_name = asm_file.file_name,
+                },
+            });
+            return error.ParseFailure;
+        }
+        return token;
+    }
+};
+
 fn assembleExecutable(assembly: *Assembly) !void {
     const cwd = fs.Dir.cwd();
 
     for (assembly.input_files) |input_file| {
-        const source = try cwd.readFileAlloc(assembly.allocator, input_file, math.maxInt(usize));
-        var tokenizer = Tokenizer.init(source);
+        var asm_file = AsmFile{
+            .assembly = assembly,
+            .file_name = input_file,
+            .sections = AsmFile.SectionTable.init(assembly.allocator),
+            .globals = AsmFile.GlobalSet.init(assembly.allocator),
+            .source = try cwd.readFileAlloc(assembly.allocator, input_file, math.maxInt(usize)),
+            .tokenizer = undefined,
+            .put_back_buffer = undefined,
+            .put_back_count = 0,
+        };
+        asm_file.tokenizer = Tokenizer.init(asm_file.source);
         while (true) {
-            const token = tokenizer.next();
+            const token = asm_file.nextToken();
             switch (token.id) {
                 .eof => break,
+                .period => {
+                    const dir_ident = try asm_file.expectToken(.identifier);
+                    const dir_name = asm_file.tokenSlice(dir_ident);
+                    if (mem.eql(u8, dir_name, "text")) {
+                        try asm_file.setCurrentSection("text");
+                    } else if (mem.eql(u8, dir_name, "globl")) {
+                        while (true) {
+                            const ident = try asm_file.expectToken(.identifier);
+                            try asm_file.addGlobal(asm_file.tokenSlice(ident));
+                            if (asm_file.eatToken(.comma)) |_| continue else break;
+                        }
+                    } else {
+                        try assembly.errors.append(.{
+                            .unrecognized_directive = .{
+                                .token = dir_ident,
+                                .source = asm_file.source,
+                                .file_name = asm_file.file_name,
+                            },
+                        });
+                        return error.ParseFailure;
+                    }
+                },
+                .identifier => {
+                    if (asm_file.eatToken(.colon)) |_| {
+                        std.debug.panic("TODO: switch to section {}\n", asm_file.tokenSlice(token));
+                    } else {
+                        std.debug.panic("TODO: handle assembly instruction {}\n", asm_file.tokenSlice(token));
+                    }
+                },
                 else => {
                     try assembly.errors.append(.{
                         .unexpected_token = .{
                             .token = token,
-                            .source = source,
-                            .file_name = input_file,
+                            .source = asm_file.source,
+                            .file_name = asm_file.file_name,
                         },
                     });
                     return error.ParseFailure;
